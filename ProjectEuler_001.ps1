0e0864235dd0e64ce6f4d4f862c3b838d8fcc10b
Louis Kiphen <greypitch@live.com>
2016-09-06 14:12:38 -0400

    Initial Commit

---
+++ ProjectEuler_001.ps1
@@ -0,0 +1,20 @@
+﻿#Project Euler Problem 1
+Param(
+  [int]$Multiple_1 = 3,
+  [int]$Multiple_2 = 5,
+  [int]$TopNumber = 100
+)
+if(($Multiple_1 -lt 1)-or($Multiple_2 -lt 1)-or($TopNumber -lt 1)){throw "Negative integers are not allowed."}
+$Multiples = @()
+
+
+$n = 1
+
+do{
+if((($n % $Multiple_1) -eq 0)-or (($n % $Multiple_2) -eq 0)){$Multiples += $n}
+$n++
+}while($n -lt $TopNumber)
+
+$Result = $Multiples | Measure-Object -Sum
+
+Return $Result.Sum
