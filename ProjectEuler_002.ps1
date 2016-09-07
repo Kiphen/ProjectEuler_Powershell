698796a0a9f0ddaec39477a16b8bd99f6b604c2b
Louis Kiphen <greypitch@live.com>
2016-09-07 13:33:59 -0400

    Added solution to problem #2

---
+++ ProjectEuler_002.ps1
@@ -0,0 +1,16 @@
+﻿#Project Euler Problem 2
+
+$UpperLimit = 4000000
+$Fib1=1
+$Fib2=2
+$FibNext = $Fib1 + $Fib2
+$Sum = 0
+
+do{
+    if(($Fib2 % 2) -eq 0){$Sum += $Fib2}
+    $FibNext = $Fib1 + $Fib2
+    $Fib1 = $Fib2
+    $Fib2 = $FibNext
+    }while($FibNext -lt $UpperLimit)
+
+Write-Output $Sum
